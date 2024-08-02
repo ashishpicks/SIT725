2df696f94bed58a157f7b26f4ba469b877905a02
ashishpicks <127196281+ashishpicks@users.noreply.github.com>
2024-08-02 14:28:32 +1000

    commit

--- Week 3/public/js/script.js
+++ Week 3/public/js/script.js
@@ -10,6 +10,32 @@ $(document).ready(function () {
 });
 
 
+// Open the modal
+function openModal() {
+    document.getElementById('simpleModal').style.display = 'block';
+  }
+  
+  // Close the modal
+  function closeModal() {
+    document.getElementById('simpleModal').style.display = 'none';
+  }
+  
+  // Close the modal when clicking outside of it
+  window.onclick = function(event) {
+    var modal = document.getElementById('simpleModal');
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+  
+  // Handle item click
+  document.querySelectorAll('.product-list li').forEach(item => {
+    item.addEventListener('click', function() {
+      alert('You selected: ' + this.textContent);
+      closeModal();
+    });
+  });
+  
 
 
 
