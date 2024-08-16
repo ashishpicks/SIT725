9413dbaa936487a4bd62d8d3b06984c6eb541762
ashishpicks <127196281+ashishpicks@users.noreply.github.com>
2024-08-16 15:22:16 +1000

    commit

--- Week 5/controllers/userController.js
+++ Week 5/controllers/userController.js
@@ -1,19 +1,6 @@
 const User=require('../models/user');
 
-// Controller to get user info json from
-/* exports.getUserData = async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.session.user.email });
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
-        res.json(user); // Send user details as JSON
-    } catch (err) {
-        console.error("Error fetching user data:", err);
-        res.status(500).send("Error fetching user data");
-    }
-}; */
-// Controller to serve user page with EJS
+
 exports.getUserData = async (req, res) => {
     try {
         const user = await User.findOne({ email: req.session.user.email });
@@ -21,7 +8,7 @@ exports.getUserData = async (req, res) => {
             return res.status(404).send("User not found");
         }
 
-        // Render the EJS template and pass user data
+        // Rendering and passing data to view->user
         res.render('user/user', { user });
     } catch (err) {
         console.error("Error fetching user data:", err);
--- Week 5/routes/userRoute.js
+++ Week 5/routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const isAuthenticated = require('../middleware/auth');
-// Route to get user info
+// Route to get user info related data
 router.get('/user', isAuthenticated, userController.getUserData);
 
 module.exports = router;
